0938e88fb45ebf9bbaaaaf08c563175059ae3b2b
WenVanesa <wvanessalomo2006@gmail.com>
2024-08-05 19:36:01 -0500

    Primero inicio

--- App.tsx
+++ App.tsx
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, TextInput, Button, StyleSheet, SafeAreaView, Image } from 'react-native';
 
-export default function App() {
+const Login: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.body}>
+      <Image style={styles.img} source={require('./assets/logo.png')}></Image>
+      <Text style={styles.titulo1}>SICOMRE</Text>
+      <View style={styles.container}>
+        <Text style={styles.title}>¡¡Ingresa!!</Text>
+        <TextInput style={styles.input} placeholder="Usuario"/>
+        <TextInput style={styles.input} placeholder="Contraseña" secureTextEntry keyboardType="visible-password" />
+        <Button title="Iniciar Sesión" color='#138d75' onPress={() => { /* manejar inicio de sesión */ }} />
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
+  body: {
+    backgroundColor: '#27ae60',
+    flex: 1,
+  },
+
+  titulo1: {
+    fontSize: 45,
+    marginLeft: 150, // Ajustar margen según necesidad
+    marginTop: -90,
+  },
+
+  img: {
+    marginTop: 35,
+    marginLeft: 15,
+    width: 125,
+    height: 125,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  input: {
+    backgroundColor: 'white',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 20,
+    paddingHorizontal: 10,
   },
 });
+
+export default Login;
